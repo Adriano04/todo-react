afa01c0444b5999e7fe8fd3c05e2beeea497ac9a
Adriano Markovic <adriano.markovic@outlook.com>
2022-04-09 22:00:16 +0200

    Add OpenApi generation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { OpenAPI, TodoService } from './api';
+
+// Proxy must be added before base URL so we avoid the CORS issue when developing locally.
+const corsProxyUrl = 'http://localhost:8081/';
+OpenAPI.BASE = corsProxyUrl + 'http://localhost:8080'
 
 function App() {
+
+  // Testing TodoService.
+  const fetchTodos = async () => {
+    const todos = await TodoService.getTodosPaged(0, 2);
+    console.log(todos);
+  }
+
+  fetchTodos().then();
+
   return (
     <div className="App">
       <header className="App-header">
